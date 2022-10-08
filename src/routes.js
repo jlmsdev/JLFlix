e51dc1bef7f05550b22aed3d23346873cd387635
JoaoLucas <joaosouza1135@gmail.com>
2022-10-08 14:42:16 -0300

    Corrigindo Routes

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route} from  'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
-import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Filme from './pages/Filme';
+
 import Erro from './pages/Erro';
 
 
